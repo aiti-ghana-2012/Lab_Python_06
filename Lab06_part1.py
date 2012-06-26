f4796b21a31b6a985ad221056ebc5503e2b7db78
Louis Sobel <sobel@lsmbp.local>
2012-06-26 10:10:02 +0000

    instructions

---
+++ Lab06_part1.py
@@ -0,0 +1,32 @@
+"""
+Lab_Python_06
+Part 1
+"""
+
+"""
+Whatever the datastructure you choose,
+it should represent the following data:
+
+player		| date		| score
+_______________________________________
+rooney		| 6/23/2012	| 2
+rooney		| 6/25/2012	| 2
+ronaldo		| 6/19/2012	| 0
+ronaldo		| 6/20/2012	| 3
+torres		| 6/21/2012	| 0
+torres		| 6/21/2012	| 1
+"""
+
+## create the player_stats data structure
+
+
+
+## implement highest_score
+
+
+
+## implement highest_score_for_player
+
+
+
+## implement highest_scorer
